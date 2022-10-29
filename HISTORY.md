a4b58dc7c44c5d7e8e92cc4134a570aa93e455bd
Mariana Gonçalves <marigonsilva@gmail.com>
2022-10-29 20:12:49 -0300

    Created Hello World app with Python

---
+++ HISTORY.md
@@ -0,0 +1,9 @@
+* Para iniciar a aplicação para testes locais, sem container:
+
+```
+cd app
+gunicorn --log-level debug app:app
+
+```
+
+* Para iniciar a aplicação para testes locais, com docker:
---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app_name = 'hello'
+app = Flask(app_name)
+app.debug = True
+
+@app.route('/')
+def api_comment_list():
+    message = 'Hello World!'
+    return message;
